eaefd228ad0e37a76a0cf9fdb3a34ff431c94d0c
Kjiw0n <rwd0904@naver.com>
2024-04-16 00:30:41 +0900

    Feat: nav목차의 카테고리 별 상품 리스트 필터링 기능 구현

---
+++ week1/hw1/scripts/index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let homeBtn = document.querySelector('.home-btn');
+    homeBtn.addEventListener('click', function() {
+        window.location.href = '/JiWonKim/week1/hw1/index.html';
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.sidebar button');
+    const sections = document.querySelectorAll('.section');
+
+    console.log(buttons);
+    console.log(sections);
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const filterType = this.classList[0]; 
+            console.log(filterType)
+            
+            sections.forEach(section => {
+                if (filterType === 'section-all') {
+                    section.style.display = 'grid';
+                } else if (section.id === filterType) {
+                    section.style.display = 'grid';
+                } else {
+                    section.style.display = 'none';
+                }
+            });
+            
+        });
+    });
+});
