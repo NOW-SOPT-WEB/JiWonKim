448a7eef7cb9e68ee98f649a23717ecc6826d161
Kjiw0n <rwd0904@naver.com>
2024-04-16 00:27:16 +0900

    Feat: 상품 데이터 분리 및 렌더링 구현

--- week1/hw1/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let homeBtn = document.querySelector('.home-btn');
-    homeBtn.addEventListener('click', function() {
-        window.location.href = '/JiWonKim/week1/hw1/index.html';
-    })
-})
---
+++ week1/hw1/scripts/shopList.js
@@ -0,0 +1,32 @@
+const SHOPPING_LIST = [
+    {
+        filter: 'section1',
+        items: [
+            { id: 1, name: '초콜릿1', price: 1000, img: 'class1/img1.jpeg' },
+            { id: 2, name: '초콜릿2', price: 1500, img: 'class1/img2.jpeg' },
+            { id: 3, name: '초콜릿3', price: 10200, img: 'class1/img3.jpeg' },
+            { id: 4, name: '초콜릿4', price: 1500, img: 'class1/img4.jpeg' },
+            { id: 5, name: '초콜릿5', price: 1000, img: 'class1/img5.jpeg' },
+            { id: 6, name: '초콜릿6', price: 1500, img: 'class1/img6.jpeg' },
+            { id: 7, name: '초콜릿7', price: 2500, img: 'class1/img7.jpeg' },
+            { id: 8, name: '초콜릿8', price: 15040, img: 'class1/img8.jpeg' }
+        ]
+    },
+    {
+        filter: 'section2',
+        items: [
+            { id: 1, name: '망곰이1', price: 1000, img: 'class2/im1.jpeg' },
+            { id: 2, name: '망곰이2', price: 1500, img: 'class2/im2.jpeg' },
+            { id: 3, name: '망곰이3', price: 10200, img: 'class2/im3.jpeg' },
+            { id: 4, name: '망곰이4', price: 1500, img: 'class2/im4.jpeg' }
+        ]
+    },
+    {
+        filter: 'section3',
+        items: [
+            { id: 1, name: '암거나1', price: 1001324560, img: 'class3/immg1.jpeg' },
+            { id: 2, name: '암거나2', price: 152500, img: 'class3/immg2.png' },
+            { id: 3, name: '암거나3', price: 1020880, img: 'class3/immg3.jpg' }
+        ]
+    }
+];
