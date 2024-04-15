9b24d0d82568a518de2fbcdbe9e40eb0d486bdaa
Kjiw0n <rwd0904@naver.com>
2024-04-15 20:37:52 +0900

    Feat: 로고 클릭 시 홈 화면으로 이동 기능 구현

---
+++ week1/hw1/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let homeBtn = document.querySelector('.home-btn');
+    homeBtn.addEventListener('click', function() {
+        window.location.href = '/JiWonKim/week1/hw1/index.html';
+    })
+})
