4b3047e94a86a16bc661fd9bbf4922cff6d4fc62
Kjiw0n <rwd0904@naver.com>
2024-05-05 17:00:13 +0900

    feat: 카드 이미지 데이터에 alt 태그 추가

--- week3/src/Main/Card/CardData.jsx
+++ week3/src/Main/Card/CardData.jsx
@@ -1,13 +1,13 @@
 const cardData = [
-    {id: 1, backImg: 'src/assets/back1.png'},
-    {id: 2, backImg: 'src/assets/back2.png'},
-    {id: 3, backImg: 'src/assets/back3.png'},
-    {id: 4, backImg: 'src/assets/back4.png'},
-    {id: 5, backImg: 'src/assets/back5.png'},
-    {id: 6, backImg: 'src/assets/back6.png'},
-    {id: 7, backImg: 'src/assets/back7.png'},
-    {id: 8, backImg: 'src/assets/back8.png'},
-    {id: 9, backImg: 'src/assets/back9.png'}
+    {id: 1, backImg: 'src/assets/back1.png', alt: 'backImg1'},
+    {id: 2, backImg: 'src/assets/back2.png', alt: 'backImg2'},
+    {id: 3, backImg: 'src/assets/back3.png', alt: 'backImg3'},
+    {id: 4, backImg: 'src/assets/back4.png', alt: 'backImg4'},
+    {id: 5, backImg: 'src/assets/back5.png', alt: 'backImg5'},
+    {id: 6, backImg: 'src/assets/back6.png', alt: 'backImg6'},
+    {id: 7, backImg: 'src/assets/back7.png', alt: 'backImg7'},
+    {id: 8, backImg: 'src/assets/back8.png', alt: 'backImg8'},
+    {id: 9, backImg: 'src/assets/back9.png', alt: 'backImg9'}
 ]
 
 export default cardData
