05d4411bd3117b4d5c1b64454d58c35dc3479d90
Kjiw0n <rwd0904@naver.com>
2024-05-07 03:15:51 +0900

    feat: api 관련 상수 데이터 파일로 생성

---
+++ week4/src/components/config.jsx
@@ -0,0 +1,8 @@
+const BASE_URL = 'http://34.64.233.12:8080';
+
+export const API = {
+  JOIN: `${BASE_URL}/member/join`,
+  INFO: `${BASE_URL}/member/info`,
+  LOGIN: `${BASE_URL}/member/login`,
+  PASSWORD: `${BASE_URL}/member/password`,
+};
